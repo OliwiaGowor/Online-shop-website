be028d713bab8b3f0d44a425036e958e6e2f4240
Oliwia Gowor <72342415+OliwiaGowor@users.noreply.github.com>
2022-09-19 22:51:54 +0200

    added dropdowns: shopping cart, languages, currency

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector('.languages').addEventListener('click', function() {
+	this.classList.toggle('open');
+}); 
+
+document.querySelector('.currency').addEventListener('click', function() {
+	this.classList.toggle('open');
+}); 
+
